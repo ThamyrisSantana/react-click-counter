9bb5047b81c9c75defb2d9b73dffaefda30b3bbf
Thamyris <thamyrissatana3@gmail.com>
2021-05-11 14:11:52 -0300

    counter

---
+++ src/components/counter/counter.js
@@ -0,0 +1,18 @@
+import React, { useState } from "react";
+import "./counter.css";
+
+const Counter = () => {
+  const [count, setCount] = useState(0);
+
+  return (
+    <div>
+      <div className="counter-box">
+        <h1>Click Counter</h1>
+        <p>{count}</p>
+        <button onClick={() => setCount(count + 1)}>Click</button>
+      </div>
+    </div>
+  );
+};
+
+export default Counter;
